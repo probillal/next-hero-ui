6c8a692e5a18e194eb6b0278a9deecf4ccfab73f
probillal <smbillalhossain273@gmail.com>
2026-06-20 10:27:54 +0600

    Using hero ui components with fetched data (card, list)

--- src/app/task/page.jsx
+++ src/app/task/page.jsx
@@ -1,11 +1,17 @@
+import TaskCard from "@/components/TaskCard";
 import { getTask } from "@/lib/task";
 import React from "react";
 
 const TaskPage = async () => {
-  const task = await getTask();
+  const tasks = await getTask();
   return (
     <div>
-      <h1 className="text-4xl font-bold mb-4">Task: {task.length} </h1>
+      <h1 className="text-4xl font-bold mb-4">Task: {tasks.length} </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {tasks.map((task) => (
+          <TaskCard key={task.id} task={task}></TaskCard>
+        ))}
+      </div>
     </div>
   );
 };
