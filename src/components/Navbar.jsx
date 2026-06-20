7c96c0d11e957f05e22a46b95a54f00886de965d
probillal <smbillalhossain273@gmail.com>
2026-06-20 11:05:57 +0600

    Navigatiobar added from hero ui

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="sticky top-0 z-40 w-full border-b border-separator bg-background/70 backdrop-blur-lg">
+      <header className="flex h-16 items-center justify-between px-6">
+        <div className="flex items-center gap-3">
+          <p className="font-bold">ACME</p>
+        </div>
+        <ul className="flex items-center gap-4">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="task">Task</Link>
+          </li>
+        </ul>
+      </header>
+    </nav>
+  );
+};
+
+export default Navbar;
